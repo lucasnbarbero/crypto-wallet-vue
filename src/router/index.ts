0f8cda29833796a2128efc40cbbdfedd6df15140
Lucas Barbero <lucasnbarbero95@gmail.com>
2023-12-12 18:42:36 -0300

    Creacion de la vista base para Login #5

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+
+import MainLayout from '@/layouts/MainLayout.vue';
+import AuthLayout from '@/layouts/AuthLayout.vue';
+
+import HomeView from '@/views/HomeView.vue';
+
+import LoginView from '@/views/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'MainLayout',
+      component: MainLayout,
+      children: [{ path: '/', name: 'Home', component: HomeView }],
+    },
+    {
+      path: '/',
+      name: 'AuthLayout',
+      component: AuthLayout,
+      children: [{ path: '/login', name: 'Login', component: LoginView }],
     },
   ],
 });
